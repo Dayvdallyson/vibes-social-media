d46e03c43db2add7120ef5fbb6691ff6a7b69bf2
Dayvdallyson <dayvdallyson21@gmail.com>
2025-05-18 22:23:29 -0300

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+  return (
+    <div className="text-3xl font-bold mx-auto flex items-center justify-center">
+      HomePage
+    </div>
+  );
+};
+
+export default HomePage;
